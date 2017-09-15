0bc7b5e38207b446055e3725d43782816a96db39
matt-sutton.github.io <matt.sutton@qut.edu.au>
2017-09-15 13:18:12 +1000

    Basic Optimisation implemented

---
+++ optimisation.m
@@ -0,0 +1,19 @@
+clear all
+%gamedata = csvread(strcat('pirate.csv'));
+
+p = 100*[0.2 0.8 0.2]';
+r = [30 30 10]';
+ts = 30;
+
+cvx_solver gurobi
+cvx_begin
+    variable x(3) integer
+    maximize( sum((1-max(p - x,0)/100).*r) )
+    subject to
+        sum(x) == ts;
+        x>=0;
+cvx_end
+
+clc
+
+sprintf('# Coin = (%d %d %d), Exp Rwd = %d \n',x',cvx_optval)
